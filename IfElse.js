8f2373f67e04062f3d64b13723c6c143dac33df8
Kaiser <bernardosouzas2005@gmail.com>
2024-09-25 18:29:41 -0300

    mae tem cafe if else

---
+++ IfElse.js
@@ -0,0 +1,11 @@
+console.log ("mãe tem café?")
+
+const temCafé = false
+
+if (temCafé){
+    console.log("Teem!")
+} else{
+    console.log("Tem Não!")
+}
+
+console.log( temCafé ? 'A alegria dentro de mim é como uma xícara de café, sempre presente no canto da mesa. O aroma que um dia foi desalentador agora se espalha pelo ar, trazendo memórias doces e acolhedoras. Cada gole me lembra do calor e da companhia que preenchem o meu corpo e alma, trazendo uma sensação de conforto e felicidade.' : 'pois vou criar um universo repleto de dor e sofrimento este mundo que estou prestes a criar será um espelho distorcido de tudo que ja vivi de cada cicatriz que carrego, vo dar a vida a um universo onde a felicidade não passa de uma lembrança distante, uma sombra fugaz que nunca se materializa, nesse universo as pessoas que me fizeram mal estarão presas em um ciclo interminável de dor, lutando contra forças que não podem vencer..')
